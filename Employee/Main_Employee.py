729f4dc179ba5ecd86fbe6c29a433096aa920017
Florin Roscan <112968630+FlorinRoscan@users.noreply.github.com>
2024-03-25 13:46:02 +0200

    Update Main_Employee.py

--- Employee/Main_Employee.py
+++ Employee/Main_Employee.py
@@ -6,10 +6,8 @@ employee3 = Employee("Bob", "Johnson", 4500, "Network Administrator", 1, 1)
 employee4 = Employee("Alice", "Lee", 5500, "Database Administrator", 4, 5)
 employee5 = Employee("Tom", "Wang", 7000, "Senior Software Developer", 6, 7)
 
-# update an employee's experience age and salary based on the new experience age
 employee2.update_experience_age(5)
 
-# print the updated employee information
 print(employee2)
 print('------------------------------------------------------------------------------------')
 print(employee3)
@@ -19,5 +17,3 @@ print('-------------------------------------------------------------------------
 print(employee5)
 print('------------------------------------------------------------------------------------')
 print(employee4)
-
-
